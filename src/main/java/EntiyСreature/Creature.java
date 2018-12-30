75056dffffcc736cd144490d3ef87d67e6f79f50
adanos911 <ivanbetsis@gmail.com>
2018-12-30 23:31:49 +0300

    add fields for Human and get/set for Creature

--- src/main/java/EntiyСreature/Creature.java
+++ src/main/java/EntiyСreature/Creature.java
@@ -2,9 +2,17 @@ package EntiyСreature;
 
 public abstract class Creature {
 
+    public boolean isLiveStage() {
+        return liveStage;
+    }
+
+    public void setLiveStage(boolean liveStage) {
+        this.liveStage = liveStage;
+    }
+
     /**
      * Поле, отвечающие за состояния существа.
      * Может принимать два значения: true - существо живо, false - существо мертво
      */
-    boolean liveStage;
+    protected boolean liveStage;
 }
--- src/main/java/EntiyСreature/Human.java
+++ src/main/java/EntiyСreature/Human.java
@@ -1,4 +1,20 @@
 package EntiyСreature;
 
 public class Human extends Creature{
+
+    /**
+     * Поле, отвечающие за количество пулей у игрока
+     */
+    private int bullets;
+
+    /**
+     * Поле, отвечающие за количество гранат у игрока
+     */
+    private int grenades;
+
+    /**
+     * Поле, отвечающие за наличие мешка цемента у игрока
+     *
+     */
+    private boolean bagOfCement;
 }
