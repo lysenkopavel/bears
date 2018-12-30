e533ea8a27671f5a69b8ab889725fa4de8eb1071
dvm <dvm@netcracker.com>
2018-12-30 23:35:21 +0300

    добавил геттеры и сеттеры игроку

--- src/main/java/EntiyСreature/Human.java
+++ src/main/java/EntiyСreature/Human.java
@@ -17,4 +17,29 @@ public class Human extends Creature{
      *
      */
     private boolean bagOfCement;
+
+
+    public int getBullets() {
+        return bullets;
+    }
+
+    public void setBullets(int bullets) {
+        this.bullets = bullets;
+    }
+
+    public int getGrenades() {
+        return grenades;
+    }
+
+    public void setGrenades(int grenades) {
+        this.grenades = grenades;
+    }
+
+    public boolean isBagOfCement() {
+        return bagOfCement;
+    }
+
+    public void setBagOfCement(boolean bagOfCement) {
+        this.bagOfCement = bagOfCement;
+    }
 }
