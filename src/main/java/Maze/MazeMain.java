7f5fe6820c890951a26e43f738f0e1f1970e9820
dvm <dvm@netcracker.com>
2018-12-31 00:35:42 +0300

    начало строительства лабиринта

--- src/main/java/Maze/MazeMain.java
+++ src/main/java/Maze/MazeMain.java
@@ -1,4 +1,17 @@
 package Maze;
 
 public class MazeMain {
+
+    private final int sizeX;
+
+    private final int sizeY;
+
+    private Wall[][] walls;
+
+    private Location[][] locations;
+
+    public MazeMain(int sizeX, int sizeY) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
 }
